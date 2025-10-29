4bc156371337309e60bd2b45a615ab66d8248bd2
aditia nugraha <aditianugraha@aditias-MacBook-Air.local>
2025-10-29 13:51:54 +0700

    sosmed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { copyFileSync } from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync('public/_redirects', 'dist/_redirects');
+      },
+    },    
     tailwindcss()
   ],
 })
